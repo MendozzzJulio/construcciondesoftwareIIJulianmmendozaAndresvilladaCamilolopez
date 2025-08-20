d8919101026d4d29da66f68a09cd258e081b6043
andres <andresfvb07@gmail.com>
2025-08-20 16:00:14 -0600

    inital commit

---
+++ construccion-2/src/main/java/app/Construccion2Application.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Construccion2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Construccion2Application.class, args);
+	}
+
+}
