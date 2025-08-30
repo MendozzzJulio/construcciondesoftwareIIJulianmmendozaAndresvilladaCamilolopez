c4e15f1d30ca48244b3af06f7a65027ed9f23bce
Lopezal00 <lopezalzatecamilo63@gmail.com>
2025-08-30 14:29:08 -0500

    add search clinical records by pet

--- software2 (2)/software2/software2/src/main/java/app/domain/models/Facture.java
+++ software2 (2)/software2/software2/src/main/java/app/domain/models/Facture.java
@@ -1,5 +1,8 @@
 package app.domain.models;
 
 public class Facture {
+	private Long idFact;
+	private Pacient paciente;
+	//private Doctor medico;
 
 }
--- software2 (2)/software2/software2/src/main/java/app/domain/models/MedicationOrden.java
+++ software2 (2)/software2/software2/src/main/java/app/domain/models/MedicationOrden.java
@@ -1,5 +1,44 @@
 package app.domain.models;
 
 public class MedicationOrden {
+	private Long orderNumber;
+	private Long patientID;
+	private Long doctorID;
+	private String date;
+	
+	
+	public MedicationOrden(Long orderNumber, Long patientID, Long doctorID, String date) {
+		super();
+		this.orderNumber = orderNumber;
+		this.patientID = patientID;
+		this.doctorID = doctorID;
+		this.date = date;
+	}
+	
+	public Long getOrderNumber() {
+		return orderNumber;
+	}
+	
+	public void setOrderNumber(Long orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+	public Long getPatientID() {
+		return patientID;
+	}
+	public void setPatientID(Long patientID) {
+		this.patientID = patientID;
+	}
+	public Long getDoctorID() {
+		return doctorID;
+	}
+	public void setDoctorID(Long doctorID) {
+		this.doctorID = doctorID;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
 
 }
